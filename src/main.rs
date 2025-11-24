fd815acdf5bb5c05d99b186e11c9f939c9ca4178
Patrick Lin <linpatrick11@gmail.com>
2025-11-23 21:35:41 -0500

    moved arg parsing to constructor in Config struct

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,20 @@ struct Config {
     file_path: String
 }
 
+impl Config {
+    fn new(args: &[String]) -> Self {
+        let q = args[1].clone();
+        let f = args[2].clone();
+        return Config { query: q, file_path: f };
+    }
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config : Config = parse_args(&args);
+    let config : Config = Config::new(&args);
 
     let contents = fs::read_to_string(config.file_path)
         .expect("should be able to read file");
     
     println!("{contents}");
-}
-
-fn parse_args(args: &[String]) -> Config {
-    return Config { query: args[1].clone(), file_path: args[2].clone() };
 }
