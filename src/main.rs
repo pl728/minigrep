3b53ec5018a55d7200b36dcc6a0dd1b7e620fb33
Patrick Lin <linpatrick11@gmail.com>
2025-11-23 21:34:13 -0500

    added args parsing to Config struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::env;
+use std::fs;
+
+struct Config {
+    query: String,
+    file_path: String
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let config : Config = parse_args(&args);
+
+    let contents = fs::read_to_string(config.file_path)
+        .expect("should be able to read file");
+    
+    println!("{contents}");
+}
+
+fn parse_args(args: &[String]) -> Config {
+    return Config { query: args[1].clone(), file_path: args[2].clone() };
 }
