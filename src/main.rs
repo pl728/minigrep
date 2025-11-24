7728b7c3fbbd8d1c9cac49ae7224231a270394dc
Patrick Lin <linpatrick11@gmail.com>
2025-11-23 23:54:52 -0500

    print errors to stderr

--- src/main.rs
+++ src/main.rs
@@ -26,15 +26,12 @@ impl Config {
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("problem parsing arguments: {err}");
+        eprintln!("problem parsing arguments: {err}");
         process::exit(1);
     });
 
-    println!("Searching for: {}", config.query);
-    println!("In file: {}\n", config.file_path);
-
     if let Err(e) = run(config) {
-        println!("Application Error: {e}");
+        eprintln!("Application Error: {e}");
         process::exit(1);
     }
 }
